972df7bb95a194ea37a3060076f918a50e9c1337
AlexDouglasstochkajpg <domnitzkij.alexander@yandex.ru>
2023-10-21 15:46:20 +0300

    Перегрузка конструктора [#173890]

--- src/main/java/ru/job4j/tracker/Item.java
+++ src/main/java/ru/job4j/tracker/Item.java
@@ -4,6 +4,32 @@ public class Item {
     private int id;
     private String name;
 
+    public Item() {
+    }
+
+    public Item(String name) {
+        this.name = name;
+    }
+
+    public Item(int id, String name) {
+        this.id = id;
+        this.name = name;
+    }
+
+    public void printInfo() {
+        System.out.println("id: " + id);
+        System.out.println("name: " + name);
+    }
+
+    public static void main(String[] args) {
+        Item first = new Item();
+        first.printInfo();
+        Item second = new Item("Jack");
+        second.printInfo();
+        Item third = new Item(25, "Jack");
+        third.printInfo();
+    }
+
     public int getId() {
         return id;
     }
