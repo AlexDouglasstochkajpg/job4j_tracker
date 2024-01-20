7267cd4d4f08d00d67fa1b155c27a2db800de112
AlexDouglasstochkajpg <domnitzkij.alexander@yandex.ru>
2024-01-20 11:58:59 +0300

    0. Map, HashMap [#212652 #474871]

--- src/main/java/ru/job4j/collection/UsageMap.java
+++ src/main/java/ru/job4j/collection/UsageMap.java
@@ -6,7 +6,10 @@ import java.util.Map;
 public class UsageMap {
     public static void main(String[] args) {
         HashMap<String, String> map = new HashMap<>();
-        map.put("ivanivanov@Yandex.ru", "Ivanov Ivan Ivanovich");
+        map.put("ivanivanov@yandex.ru", "Ivanov Ivan Ivanovich");
+        map.put("alexeyalexeev@yandex.ru", "Alexeev Alexey Alexeevich");
+        map.put("alexanderalexandrov@yandex.ru", "Alexandrov Alexander Alexandrovich");
+        map.put("ivanivanov@yandex.ru", "Ivanov Ivan Petrovich");
         for (String key : map.keySet()) {
             String value = map.get(key);
             System.out.println(key + " : " + value);
